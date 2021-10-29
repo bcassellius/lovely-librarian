dbdc6713bc62d9d341d79c5284b3f5aaf9eeed5d
Colton Watkins <83984833+ThManHimself@users.noreply.github.com>
2021-10-29 17:43:23 -0500

    spacing; imported StaffDirectory

--- client/src/App.js
+++ client/src/App.js
@@ -6,46 +6,48 @@ import { setContext } from '@apollo/client/link/context'
 // components
 // import StaffSchedule from './components/StaffSchedule';
 // import RequestTimeOff from './components/RequestTimeOff';
+import StaffDirectory from './components/StaffDirectory';
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
 
 const httpLink = createHttpLink({
-  uri: "/graphql",
+	uri: "/graphql",
 });
 
 const authLink = setContext((_, { headers }) => {
-  const token = localStorage.getItem('id_token');
-  return {
-    headers: {
-      ...headers,
-      authorization: token ? `Bearer ${token}` : '',
-    },
-  };
+	const token = localStorage.getItem('id_token');
+	return {
+		headers: {
+			...headers,
+			authorization: token ? `Bearer ${token}` : '',
+		},
+	};
 });
 
 const client = new ApolloClient({
-  link: authLink.concat(httpLink),
-  cache: new InMemoryCache(),
+	link: authLink.concat(httpLink),
+	cache: new InMemoryCache(),
 });
 
 function App() {
-  return (
-    <ApolloProvider client={client}>
-      <Router>
-        <>
-          <Navbar />
-          <Switch>
-            <Route exact path='/' component={ SearchBooks } />
-            <Route exact path='/saved' component={ SavedBooks } />
-            {/* <Route exact path='/schedule' component={ StaffSchedule } /> */}
-            {/* <Route exact path='/timeOffRequest' component={ RequestTimeOff } /> */}
-            <Route render={() => <h1 className='display-2'>Wrong page!</h1>} />
-          </Switch>
-        </>
-      </Router>
-    </ApolloProvider>
-  );
+	return (
+		<ApolloProvider client={client}>
+			<Router>
+				<>
+					<Navbar />
+					<Switch>
+						<Route exact path='/' component={ SearchBooks } />
+						<Route exact path='/saved' component={ SavedBooks } />
+						{/* <Route exact path='/schedule' component={ StaffSchedule } /> */}
+						<Route exact path='/staff' component={ StaffDirectory } />
+						{/* <Route exact path='/timeOffRequest' component={ RequestTimeOff } /> */}
+						<Route render={() => <h1 className='display-2'>Wrong page!</h1>} />
+					</Switch>
+				</>
+			</Router>
+		</ApolloProvider>
+	);
 }
 
 export default App;
