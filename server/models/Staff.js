348e9f015f1bf61b6e862e106f3c64f5b4fece23
Colton Watkins <83984833+ThManHimself@users.noreply.github.com>
2021-10-29 21:15:52 -0500

    turned staffSchema into its own model

--- server/models/Staff.js
+++ server/models/Staff.js
@@ -1,4 +1,4 @@
-const { Schema } = require('mongoose');
+const { Schema, model } = require('mongoose');
 
 const staffSchema = new Schema({
 
@@ -27,4 +27,6 @@ const staffSchema = new Schema({
     }
 });
 
-module.exports = staffSchema;
+const Staff = model('Staff', staffSchema);
+
+module.exports = Staff;
