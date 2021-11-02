cbb6d2da3985eabe5c50e817abe2a717fb3137ac
Bri <bhilliker@gmail.com>
2021-11-01 19:12:45 -0500

    add calendar events

--- client/src/components/Calendar/index.js
+++ client/src/components/Calendar/index.js
@@ -2,26 +2,61 @@ import React from 'react';
 import FullCalendar from '@fullcalendar/react';
 import daygridPlugin from '@fullcalendar/daygrid';
 import interactionPlugin from '@fullcalendar/interaction';
+import timegridPlugin from '@fullcalendar/timegrid';
 
 function Calendar() {
   const handleDateClick = (dateClickInfo)=>{
-    console.log(dateClickInfo.dateStr);
+    alert(dateClickInfo.dateStr);
   }
 
   const data = [
     {
-      title:"Give Dogs Flea Meds",
-      start: "2021-10-01"
+      title:"Costume Parade",
+      start: "2021-10-31"
     },
     {
-      title: "Finish Project",
+      title: "10a Story Time",
       start: "2021-11-03"
+    },
+    {
+      title: "11a Staff Meeting",
+      start: "2021-11-01"
+    },
+    {
+      title: "10a Story Time",
+      start: "2021-11-03"
+    },
+    {
+      title: "Author Visit",
+      start: "2021-11-03"
+    },
+    {
+      title: "Book Signing",
+      start: "2021-11-04"
+    },
+    {
+      title: "Book Club",
+      start: "2021-11-05"
+    },
+    {
+      title: "10a Story Time",
+      start: "2021-11-10"
+    },
+    {
+      title: "10a Story Time",
+      start: "2021-11-17"
+    },
+    {
+      title: "10a Story Time",
+      start: "2021-11-24"
     }
   ]
   return (
     <FullCalendar
       events={data}
-      plugins={[daygridPlugin, interactionPlugin]} 
+      plugins={[daygridPlugin, timegridPlugin, interactionPlugin]} 
+      editable={true}
+      selectable={true}
       dateClick={handleDateClick}
     />
   );
