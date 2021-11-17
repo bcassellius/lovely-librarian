b00b9da40b21d309951f4665128a3c147b51a78b
Bri <bhilliker@gmail.com>
2021-11-17 14:15:57 -0600

    login error message

--- client/src/pages/LoginForm.js
+++ client/src/pages/LoginForm.js
@@ -56,7 +56,7 @@ const LoginForm = () => {
 					show={showAlert}
 					variant="danger"
 				>
-					Something went wrong with your login credentials!
+					Something went wrong with your login!
 				</Alert>
 				<Form.Group>
 					<Form.Label htmlFor="email">Email</Form.Label>
